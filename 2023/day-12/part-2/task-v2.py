ad0f3e3b1feb4900a35ce370cc84c90c1e587d6f
MikBac <mbachorz1997@gmail.com>
2023-12-13 19:03:52 +0100

    add cache to day 12

--- 2023/day-12/part-2/task-v2.py
+++ 2023/day-12/part-2/task-v2.py
@@ -11,12 +11,16 @@ def main():
 			schema = "?".join([schema] * 5)
 			requirements = requirements * 5
 
-			response += find(schema, requirements, 0, 0, 0)
+			response += find(schema, requirements, {}, 0, 0, 0)
 
 	print('Answer: {}'.format(response))
 
 
-def find(schema, requirements, current_position, current_block_position, block_size):
+def find(schema, requirements, cache, current_position, current_block_position, block_size):
+	cache_key = (current_position, current_block_position, block_size)
+	if cache_key in cache:
+		return cache[cache_key]
+
 	if current_position == len(schema):
 		if current_block_position == len(requirements) and block_size == 0:
 			return 1
@@ -26,17 +30,17 @@ def find(schema, requirements, current_position, current_block_position, block_s
 			return 0
 
 	sum = 0
-
 	if schema[current_position] == '.' or schema[current_position] == '?':
 		if block_size == 0:
-			sum += find(schema, requirements, current_position + 1, current_block_position, 0)
-		elif (block_size > 0 and
-		      current_block_position < len(requirements) and
-		      requirements[current_block_position] == block_size):
-			sum += find(schema, requirements, current_position + 1, current_block_position + 1, 0)
+			sum += find(schema, requirements, cache, current_position + 1, current_block_position, 0)
+		elif block_size > 0:
+			if current_block_position < len(requirements) and requirements[current_block_position] == block_size:
+				sum += find(schema, requirements, cache, current_position + 1, current_block_position + 1, 0)
 
 	if schema[current_position] == '#' or schema[current_position] == '?':
-		sum += find(schema, requirements, current_position + 1, current_block_position, block_size + 1)
+		sum += find(schema, requirements, cache, current_position + 1, current_block_position, block_size + 1)
+
+	cache[cache_key] = sum
 
 	return sum
 
