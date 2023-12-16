f120f7574db69f031a5dab61abafd75b7e65d8e9
MikBac <mbachorz1997@gmail.com>
2023-12-16 13:29:53 +0100

    add part-1 day 16

--- 2023/day-16/part-1/task.py
+++ 2023/day-16/part-1/task.py
@@ -1,50 +1,101 @@
 # Created by MikBac on 2023
 
+import copy
+import sys
 
 map = []
+visited_map = []
 
 
 def main():
 	global map
+	global visited_map
+
 	sum = 0
-	with open('sample', 'r', encoding='utf-8') as input_:
+
+	with open('input', 'r', encoding='utf-8') as input_:
 		for row, line in enumerate(input_):
 			map.append(list('+' + line.strip() + '+'))
+
+	sys.setrecursionlimit(len(map) * len(map[0]))
 
 	map.append(list('+' * len(map[0])))
 	map.insert(0, list('+' * len(map[0])))
 
-	print(map)
+	visited_map = copy.deepcopy(map)
 
-	sum = go([1, 1], 1, [1, 2])
+	go([1, 1], 1, '>', [])
+
+	for i in range(len(visited_map)):
+		sum += visited_map[i].count('#')
 
 	print('Answer: {}'.format(sum))
 
 
-def go(current_position, steps, trend):
+def go(current_position, steps, direction, visited):
 	global map
+	global visited_map
 
 	if map[current_position[0]][current_position[1]] == '+':
-		return steps
+		return
+
+	visited.append(current_position)
+
+	if steps > 10:
+		for v in range(len(visited) - 3):
+			if visited[v] == visited[-2] and visited[v + 1] == visited[-1]:
+				return
+
+	visited_map[current_position[0]][current_position[1]] = '#'
 
 	if map[current_position[0]][current_position[1]] == '.':
-		new_trend = []
 
-		if current_position[0] != trend[0]:
-			if current_position[0] > trend[0]:
-				new_trend = [trend[0] - 1, trend[1]]
-			else:
-				new_trend = [trend[0] + 1, trend[1]]
-		else:
-			if current_position[1] > trend[1]:
-				new_trend = [trend[0], trend[1] - 1]
-			else:
-				new_trend = [trend[0], trend[1] + 1]
-		return go(trend, steps + 1, new_trend)
+		if direction == '>':
+			go([current_position[0], current_position[1] + 1], steps + 1, direction, visited)
+		if direction == '<':
+			go([current_position[0], current_position[1] - 1], steps + 1, direction, visited)
+		if direction == '^':
+			go([current_position[0] - 1, current_position[1]], steps + 1, direction, visited)
+		if direction == 'v':
+			go([current_position[0] + 1, current_position[1]], steps + 1, direction, visited)
+
+	if map[current_position[0]][current_position[1]] == '\\':
+		if direction == '>':
+			go([current_position[0] + 1, current_position[1]], steps + 1, 'v', visited)
+		if direction == '<':
+			go([current_position[0] - 1, current_position[1]], steps + 1, '^', visited)
+		if direction == '^':
+			go([current_position[0], current_position[1] - 1], steps + 1, '<', visited)
+		if direction == 'v':
+			go([current_position[0], current_position[1] + 1], steps + 1, '>', visited)
+
+	if map[current_position[0]][current_position[1]] == '/':
+		if direction == '>':
+			go([current_position[0] - 1, current_position[1]], steps + 1, '^', visited)
+		if direction == '<':
+			go([current_position[0] + 1, current_position[1]], steps + 1, 'v', visited)
+		if direction == '^':
+			go([current_position[0], current_position[1] + 1], steps + 1, '>', visited)
+		if direction == 'v':
+			go([current_position[0], current_position[1] - 1], steps + 1, '<', visited)
 
 	if map[current_position[0]][current_position[1]] == '|':
+		if direction == '>' or direction == '<':
+			go([current_position[0] - 1, current_position[1]], steps + 1, '^', visited)
+			go([current_position[0] + 1, current_position[1]], steps + 1, 'v', visited)
+		if direction == '^':
+			go([current_position[0] - 1, current_position[1]], steps + 1, '^', visited)
+		if direction == 'v':
+			go([current_position[0] + 1, current_position[1]], steps + 1, 'v', visited)
 
-
+	if map[current_position[0]][current_position[1]] == '-':
+		if direction == '^' or direction == 'v':
+			go([current_position[0], current_position[1] + 1], steps + 1, '>', visited)
+			go([current_position[0], current_position[1] - 1], steps + 1, '<', visited)
+		if direction == '>':
+			go([current_position[0], current_position[1] + 1], steps + 1, '>', visited)
+		if direction == '<':
+			go([current_position[0], current_position[1] - 1], steps + 1, '<', visited)
 
 
 if __name__ == "__main__":
