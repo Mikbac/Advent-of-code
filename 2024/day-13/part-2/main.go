44b419e9f73b479bb16d9bfdc8e20faebf05bd12
MikBac <mbachorz1997@gmail.com>
2024-12-15 03:08:33 +0100

    solution improvement 2024/day-13

--- 2024/day-13/part-2/main.go
+++ 2024/day-13/part-2/main.go
@@ -37,23 +37,21 @@ func findOptimal(bA button, bB button, eX int, eY int) int {
 	// x * 26 + y * 67 = 12748
 	// x * 66 + y * 21 = 12176
 	// https://www.wolframalpha.com/input?i=Assuming+a+system+of+two+equations&assumption=%7B%22F%22%2C+%22SolveSystemOf2EquationsCalculator%22%2C+%22equation1%22%7D+-%3E%22x*26%2By*67%3D12748%22&assumption=%7B%22F%22%2C+%22SolveSystemOf2EquationsCalculator%22%2C+%22equation2%22%7D+-%3E%22x*66%2By*21%3D12176%22
-	// ////////////////////////////
-	//      84 * 10000000004245 - 21 * 10000000005634
-	// x1 = ----------------------------------------- = 245901639437.2623
-	//      40 * 84 - 38 * 21
-	x1 := float64(bB.y*eX-bB.x*eY) / float64(bA.x*bB.y-bA.y*bB.x)
 
-	//      40 * 10000000005634 - 38 * 10000000004245
-	// x2 = ----------------------------------------- = 7806401274.0241995
-	//      40 * 84 - 38 * 21
-	x2 := float64(bA.x*eY-bA.y*eX) / float64(bA.x*bB.y-bA.y*bB.x)
+	det := float64(bA.x)*float64(bB.y) - float64(bA.y)*float64(bB.x)
+
+	detX := float64(eX)*float64(bB.y) - float64(eY)*float64(bB.x)
+	detY := float64(bA.x)*float64(eY) - float64(bA.y)*float64(eX)
+
+	x := detX / det
+	y := detY / det
 
 	// 245901639437.2623 == 245901639437 -> false ||  7806401274.0241995 == 7806401274 -> false
-	if x1 == float64(int(x1)) || x2 == float64(int(x2)) {
+	if x != float64(int(x)) || y != float64(int(y)) {
 		return 0
 	}
 
-	return int(x1*3) + int(x2)
+	return int(x*3) + int(y)
 }
 
 func extractButton(l string) button {
